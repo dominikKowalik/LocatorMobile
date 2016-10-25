3c544cce30cc7e2b3a02503a14c6c4c378c6bdba
dominikKowalik <domocow@wp.pl>
2016-10-26 01:03:54 +0200

    5

--- app/src/main/java/com/kowalik/dominik/locatormobile/EndpointInterface.java
+++ app/src/main/java/com/kowalik/dominik/locatormobile/EndpointInterface.java
@@ -3,11 +3,15 @@ package com.kowalik.dominik.locatormobile;
 import com.kowalik.dominik.model.LocationInfo;
 import com.kowalik.dominik.model.User;
 
+import java.util.List;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
@@ -19,8 +23,20 @@ public interface EndpointInterface{
 
 
     @GET("/locator/user")
-    Call<User> getLocation();
+    Call<List<User>> getAllUsers();
 
     @POST("/locator/user")
-    Call<Void> setLocation(@Body User user);
+    Call<Void> insertUser(@Body User user);
+
+    @GET("/locator/user/{id}")
+    Call<User> getUser(@Path("id") long id);
+
+    @GET("/locator/user/{name}/{lastName}/{emailAdress}")
+    Call<User> getUser(@Path("name") String name, @Path("lastName") String lastName, @Path("emailAdress") String emailAdress);
+
+    @DELETE("/locator/user")
+    Call<Void> deleteAllUsers();
+
+    @DELETE("/locator/user/{id}")
+    Call<Void> deleteUser(@Path("id") long id);
 }
--- app/src/main/java/com/kowalik/dominik/model/User.java
+++ app/src/main/java/com/kowalik/dominik/model/User.java
@@ -1,5 +1,10 @@
 package com.kowalik.dominik.model;
 
+import com.kowalik.dominik.model.LocationInfo;
+import com.kowalik.dominik.model.UserInterface;
+
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,41 +15,58 @@ public class User implements UserInterface {
     private String emailAdress;
     private String name;
     private String lastName;
+    private String statement;
 
-    public User(String emailAdress, String lastName, String name,LocationInfo locationInfo, List<User> friends ) {
-        this.emailAdress = emailAdress;
-        this.locationInfo = locationInfo;
-        this.friends = friends;
-        this.lastName = lastName;
+    public User(String name, String lastName, String emailAdress, String password) {
         this.name = name;
+        this.lastName = lastName;
+        this.emailAdress = emailAdress;
+        this.password = password;
     }
 
-    public List<User> getFriends() {
-        return friends;
+    public String getPassword() {
+        return password;
     }
 
-    public void setFriends(List<User> friends) {
-        this.friends = friends;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    public void updateUser(User user) {
+        this.emailAdress = user.getEmailAdress();
+        this.friends = new ArrayList<>(user.getFriends());
+        this.lastName = user.getLastName();
+        this.name = user.getName();
+        this.locationInfo.setLatitude(user.getLocationInfo().getLatitude());
+        this.locationInfo.setLongitude(user.getLocationInfo().getLongitude());
+        this.statement = user.getStatement();
+        this.password = user.getPassword();
     }
 
     private List<User> friends;
-   // private boolean exists;
-
-//    public boolean isExists() {
-//        return exists;
-//    }
-//
-//    public void setExists(boolean exists) {
-//        this.exists = exists;
-//    }
 
     private LocationInfo locationInfo;
 
     private long id;
 
-    public  User(){
-    }
+//    public static float distFrom(User user1, User user2) {
+//        double earthRadius = 6371000; //meters
+//        double dLat = Math.toRadians(user2.getLocationInfo().getLatitude()-user1.getLocationInfo().getLatitude());
+//        double dLng = Math.toRadians(user2.getLocationInfo().getLongitude()-user2.getLocationInfo().getLongitude());
+//        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+//                Math.cos(Math.toRadians(user1.getLocationInfo().getLatitude())) *
+//                        Math.cos(Math.toRadians(user2.getLocationInfo().getLongitude())) *
+//                        Math.sin(dLng/2) * Math.sin(dLng/2);
+//        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+//        float dist = (float) (earthRadius * c);
+//        return dist;
+//    }
 
+
+    public User() {
+    }
 
     public String getEmailAdress() {
         return emailAdress;
@@ -74,10 +96,6 @@ public class User implements UserInterface {
         return locationInfo;
     }
 
-    public void setLocationInfo(LocationInfoInterface locationInfo) {
-
-    }
-
     public void setLocationInfo(LocationInfo locationInfo) {
         this.locationInfo = locationInfo;
     }
@@ -90,16 +108,40 @@ public class User implements UserInterface {
         this.id = id;
     }
 
-
     @Override
     public String toString() {
-        return "User{" +
-                "emailAdress='" + emailAdress + '\'' +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", friends=" + friends +
-                ", locationInfo=" + locationInfo +
-                ", id=" + id +
-                '}';
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("emailAdress='").append(emailAdress).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", locationInfo=").append(locationInfo);
+        sb.append(", id=").append(id);
+        sb.append('}');
+        return sb.toString();
     }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public void setStatement(String statement) {
+        this.statement = statement;
+    }
+
+    public List<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<User> friends) {
+        this.friends = friends;
+    }
+
+// TODO
+//    public boolean isExists() {
+//        return exists;
+//    }
+//
+//    public void setExists(boolean exists) {
+//        this.exists = exists;
+//    }
 }
